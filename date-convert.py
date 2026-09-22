6b1f597687660606abd21bde589bcf3e0b1c45e0
Daniel Lim <daniellim@OCID-JVK4V90T7C.local>
2026-09-21 14:15:36 -1000

    updated fuel price CSV

--- date-convert.py
+++ date-convert.py
@@ -1,8 +1,8 @@
 import csv
 from datetime import datetime
 
-input_file = "consumer_price_index_US.csv"
-output_file = "consumer_price_index_US_ordered.csv"
+input_file = "fuel_price_US.csv"
+output_file = "fuel_price_US_ordered.csv"
 date_column = "observation_date"
 date_format = "%Y-%m-%d" #"%Y-%m-%d %H:%M:%S"
 
